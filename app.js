ec6093fad808e11a4c25e6df128c7124c4bc621c
Micah <micahnyoung95@gmail.com>
2022-08-03 14:06:33 -0500

    GraphiQL types, schemas and resolvers simple set up

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 //generates schema
 const { buildSchema } = require("graphql");
+
 const app = express();
 
+const events = [];
 app.use(bodyParser.json());
 
 //sets where to find schemas and resolvers
@@ -12,11 +14,24 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
         type RootQuery {
-            events: [String!]
+            events: [Event!]!
         }
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
         schema {
             query: RootQuery
@@ -26,11 +41,18 @@ app.use(
     //resolver functions:
     rootValue: {
       events: () => {
-        return ["Romantic Cooking", "Sailing", "All night coding"];
+        return events;
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date,
+        };
+        events.push(event);
+        return event;
       },
     },
     graphiql: true,
