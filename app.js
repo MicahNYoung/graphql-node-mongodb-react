4141c818b74d06f445a99ae9127c4cb45e882189
Micah <micahnyoung95@gmail.com>
2022-08-05 20:54:41 -0500

    events being added with user id, but not dynamically

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const { graphqlHTTP } = require("express-graphql");
 //generates schema
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const Event = require("./models/event");
+const User = require("./models/user");
 const app = express();
 
 app.use(bodyParser.json());
@@ -22,17 +24,30 @@ app.use(
             price: Float!
             date: String!
         }
+        type User {
+          _id: ID!
+          email: String!
+          password: String
+        }
         input EventInput {
             title: String!
             description: String!
             price: Float!
             date: String!
         }
+
+        input UserInput {
+          email: String!
+          password: String!
+        }
+
         type RootQuery {
             events: [Event!]!
         }
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
+
         }
         schema {
             query: RootQuery
@@ -60,19 +75,61 @@ app.use(
           description: args.eventInput.description,
           price: +args.eventInput.price,
           date: new Date(args.eventInput.date),
+          creator: "62edc6073e148a51a11b439e",
         });
+        let createdEvent;
         return event
           .save()
           .then((result) => {
+            createEvent = { ...result._doc, _id: result._doc._id.toString() };
+            return User.findById("62edc6073e148a51a11b439e");
             console.log(result);
             //returns only info related to the object and NOT meta data
-            return { ...result._doc };
+          })
+          .then((user) => {
+            if (!user) {
+              throw new Error("User not found!");
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then((result) => {
+            return createdEvent;
           })
           .catch((err) => {
             console.log(err);
             throw err;
           });
-        return event;
+      },
+      createUser: (args) => {
+        //checks if email is already in DB
+        return User.findOne({ email: args.userInput.email })
+          .then((user) => {
+            if (user) {
+              throw new Error("User exists already!");
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then((hashedPassword) => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword,
+            });
+            return user
+              .save()
+              .then((result) => {
+                console.log(result);
+                return { ...result._doc, password: null, _id: result.id };
+              })
+              .catch((err) => {
+                console.log(err);
+                throw err;
+              });
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
       },
     },
     graphiql: true,
